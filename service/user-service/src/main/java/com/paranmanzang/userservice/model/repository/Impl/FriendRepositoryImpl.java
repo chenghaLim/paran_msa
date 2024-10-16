4cbc7db584c43a6e7c2268d74260cb0f0d018253
cheonghaLim <thddns12369@naver.com>
2024-10-16 13:12:01 +0900

    ...

--- service/user-service/src/main/java/com/paranmanzang/userservice/model/repository/Impl/FriendRepositoryImpl.java
+++ service/user-service/src/main/java/com/paranmanzang/userservice/model/repository/Impl/FriendRepositoryImpl.java
@@ -34,8 +34,8 @@ public class FriendRepositoryImpl implements FriendRepositoryCustom {
                         .select(Projections.constructor(
                                 FriendModel.class,
                                 friends.id,
-                                friends.requestUser,
                                 friends.responseUser,
+                                friends.requestUser,
                                 friends.request_at,
                                 friends.response_at))
                         .from(friends)
